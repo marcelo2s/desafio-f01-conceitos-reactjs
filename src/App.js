f444a09248a4d24493525e947f08811a80da095a
Marcelo Junior <kronussigma@gmail.com>
2020-04-21 23:47:24 -0300

    enable add/remove repository by front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `New repository ${Date.now()}`,
+      url: "https://github.com",
+      techs: ['Tech1', 'Tech2']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const endpoint = '/repositories/' + id;
+
+    await api.delete(endpoint);
+
+    const filteredRepositories = repositories.filter(item => item.id !== id);
+    
+    setRepositories(filteredRepositories);
   }
 
   return (
     <div>
+      <h1>Repositories</h1>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { 
+          repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ) 
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
